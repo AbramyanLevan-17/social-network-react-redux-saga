30b01d048e2a1b84b2303e8cebfee59f28e9ed4e
LeviosaAbrpamyan <labramyan17@gmail.com>
2020-10-30 14:21:49 +0300

    fix/login redirect

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -3,13 +3,14 @@ import { LOG_OUT, SIGN_IN } from "../types"
 const initialState = {
   isAuthorized: false,
   data:{},
-  headers:{},
 }
 
 export const authReducer = (state = initialState, action) =>{
     switch(action.type){
       case SIGN_IN:
-        return {...state, data:action.res.res.data, isAuthorized:true, headers: action.res.headers}
+        if(action.res.data){
+          return {...state, data:action.res.data, isAuthorized:true}
+        }
         case LOG_OUT:
           localStorage.removeItem('headers')
           return initialState
--- src/redux/sagas/authSaga.js
+++ src/redux/sagas/authSaga.js
@@ -1,34 +1,43 @@
-import { put, takeEvery,call } from 'redux-saga/effects'
-import {REQUEST_SIGN_IN, SIGN_IN} from '../types'
+import { put, takeEvery, call } from 'redux-saga/effects'
+import { REQUEST_SIGN_IN, SIGN_IN } from '../types'
 
 
-export function* authWatcher(){
-    yield takeEvery(REQUEST_SIGN_IN,authWorker)
+export function* authWatcher() {
+  yield takeEvery(REQUEST_SIGN_IN, authWorker)
 }
 
-function* authWorker(action){
-    const res =  yield call(fetchUser,action)
-    yield put({type:SIGN_IN,res})
+function* authWorker(action) {
+  const res = yield call(fetchUser, action)
+  yield put({ type: SIGN_IN, res })
 }
 
-async function fetchUser(action){
-    const headers = {
-            "Content-Type": "application/json",                                                                                                
-            "Access-Control-Origin": "*"
-           }
-    const response = await fetch('https://postify-api.herokuapp.com/auth/sign_in',{
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(action),
-    })
-    const data = {res: await response.json(),
-    headers:{
-        'access-token':response.headers.get('access-token'),
-        'client':response.headers.get('client'),
-        'uid':response.headers.get('uid'),
-        'content-type': 'application/json',
-        'access-control-allow-origin': '*',
-      }}
-      localStorage.setItem('headers',JSON.stringify(data.headers))
-   return  data;
+async function fetchUser(action) {
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Origin": "*"
+  }
+  const response = await fetch('https://postify-api.herokuapp.com/auth/sign_in', {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(action),
+  })
+  .then((response) => {
+    if (response.ok) {
+      return response;
+    } else {
+      throw new Error('Invalid email or password');
+    }
+  }).catch(error=>{
+    alert(error.message)
+  })
+
+  const setHeaders = {
+    'access-token': response.headers.get('access-token'),
+    'client': response.headers.get('client'),
+    'uid': response.headers.get('uid'),
+    'content-type': 'application/json',
+    'access-control-allow-origin': '*',
+  }
+  localStorage.setItem('headers', JSON.stringify(setHeaders))
+  return await response.json()
 }
